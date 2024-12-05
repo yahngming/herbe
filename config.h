8e6495081726e73ee7afdfa1e17887e8d642c419
Yang Ming <yahngming@gmail.com>
2024-12-06 01:46:28 +0800

    Update config.h

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const unsigned int padding = 15;
 static const unsigned int width = 450;
 static const unsigned int border_size = 2;
 static const unsigned int pos_x = 30;
-static const unsigned int pos_y = 30;
+static const unsigned int pos_y = 60;
 
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
-enum corners corner = TOP_RIGHT;
+enum corners corner = BOTTOM_RIGHT;
 
 static const unsigned int duration = 5; /* in seconds */
 
