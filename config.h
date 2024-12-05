79fffe976be5482a7726fc382524a8f0bbf7eb53
Yang Ming <yahngming@gmail.com>
2024-12-06 01:16:52 +0800

    Update config.h

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
-static const char *background_color = "#3e3e3e";
-static const char *border_color = "#ececec";
-static const char *font_color = "#ececec";
-static const char *font_pattern = "monospace:size=10";
+static const char *background_color = "#222222";
+static const char *border_color = "#005577";
+static const char *font_color = "#bbbbbb";
+static const char *font_pattern = "monospace:size=16";
 static const unsigned line_spacing = 5;
 static const unsigned int padding = 15;
 
